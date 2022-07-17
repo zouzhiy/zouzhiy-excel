6577f1ff79f0fbe716501db4934d8b1b3018a123
zouzhiy <546963897@qq.com>
2022-07-17 17:25:55 +0800

    添加注释

--- src/main/java/io/github/zouzhiy/excel/write/WorkbookWrite.java
+++ src/main/java/io/github/zouzhiy/excel/write/WorkbookWrite.java
@@ -19,17 +19,41 @@ import io.github.zouzhiy.excel.metadata.config.ExcelClassConfig;
 import java.util.List;
 
 /**
+ * workbook读取，
+ * 从模板读取，或者创建新的{@link org.apache.poi.ss.usermodel.Workbook}
+ * 并写入到输出流中
  * @author zouzhiy
  * @since 2022/7/2
  */
 public interface WorkbookWrite extends AutoCloseable {
 
+    /**
+     * workbook读取参数及配置上下文
+     * @return WorkbookContext
+     */
     WorkbookContext getWorkbookContext();
 
+    /**
+     * 对象配置，即对象数据转成excel表格的描述定义
+     * @return ExcelClassConfig
+     */
     ExcelClassConfig getExcelClassConfig();
 
+    /**
+     * 数据写入workbook中
+     * 此处写完后 直接将workbook写入到输出流并关闭workbook及相关的输入输出流
+     * @param itemList 数据列表
+     * @param <T> 对象类型
+     */
     <T> void write(List<T> itemList);
 
+    /**
+     * 提交
+     * <pre>
+     *     workbook写入输出流
+     *     闭workbook及相关的输入输出流
+     * </pre>
+     */
     void commit();
 
 }
